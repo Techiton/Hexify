822b4819a58adbedac7997893d07e963c9f709b4
Techiton <dynamicapidev@gmail.com>
2022-11-24 23:05:16 +1100

    Fourth Commit: Added NavBar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,33 @@
 export default function Index() {
   return (
     <div>
-      <div className="NavBar">
-        <img className="h-16 mt-4 ml-4 rounded-full" src="/Hexify.png"></img>
-        <div className="">
-
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex items-center text-gray-900 mb-4 md:mb-0">
+            <img className="w-44 p-2 rounded-full" src="Hexify.png" />
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-xl justify-center">
+            <a className="ml-10 font-NavBarFont font-semibold text-green-900">
+              Home
+            </a>
+            <a className="ml-10 font-NavBarFont font-semibold text-green-900">
+              Discover Talent
+            </a>
+            <a className="ml-10 font-NavBarFont font-semibold text-green-900">
+              Explore Jobs
+            </a>
+            <a className="ml-10 font-NavBarFont font-semibold text-green-900">
+              Hexify+
+            </a>
+          </nav>
+          <button className="inline-flex items-center border-2 border-green-900 rounded-full py-1 px-3 focus:outline-none text-lg font-semibold text-green-900 tracking-widest font-NavBarFont mt-4 md:mt-0">
+            Signup
+          </button>
+          <button className="ml-6 inline-flex items-center border-2 border-green-900 rounded-full py-1 px-3 focus:outline-none text-lg font-semibold text-green-900 tracking-widest font-NavBarFont mt-4 md:mt-0">
+            Login
+          </button>
         </div>
-      </div>
+      </header>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        NavBarFont: ['Noto Serif Khojki', "serif"]
+      }
+    },
   },
   plugins: [],
 }
